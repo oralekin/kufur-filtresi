d78bb1c89dc7eb02c255d1486e0e7d1b9a0f2780
Ekin Oral <oralekin@gmail.com>
2019-03-21 19:49:05 +0300

    embed txts in for js

--- index.js
+++ index.js
@@ -1,6 +1,102 @@
 const fs = require("fs");
+const hard = `ambiti
+amcık
+amck
+amık
+amını
+amına
+amında
+amsalak
+dalyarak
+daşak
+daşağı
+daşşak
+daşşağı
+domal
+fahişe
+folloş
+fuck
+gavat
+godoş
+göt
+hasiktir
+hassiktir
+ibne
+ipne
+kahpe
+kahbe
+kaltak
+kaltağ
+kancık
+kancığ
+kavat
+kerane
+kerhane
+kevaşe
+mastırbasyon
+masturbasyon
+mastürbasyon
+orosbu
+orospu
+orusbu
+oruspu
+orsp
+pezevenk
+pzvnk
+puşt
+qavat
+sakso
+sıçar
+sıçayım
+sıçmak
+sıçsın
+sikem
+siker
+sikeyim
+sikici
+sikik
+sikim
+sikiş
+sikle
+sikme
+siktir
+sktr
+siktiği
+sokarım
+sokayım
+sürtük
+sperm
+taşak
+taşağa
+taşağı
+taşşak
+taşşağa
+taşşağı
+vajina
+yalaka
+yarağ
+yarra
+yrrk`
 
-const vals = Array.prototype.concat(fs.readFileSync("./hard.txt",{encoding: "UTF-8"}).split("\n"),fs.readFileSync("./soft.txt",{encoding: "UTF-8"}).split("\n"))
+const soft = `am
+aq
+a.q
+a.q.
+amk
+çük
+döl
+meme
+oç
+o.ç
+o.ç.
+oral
+penis
+piç
+pipi
+sik
+yarak`
+
+const vals = Array.prototype.concat(hard.split("\n"),soft.split("\n"));
 
 // console.log(vals);
 module.exports = vals;
